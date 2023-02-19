bacbb5f94edd9f28fd1de6683575e2d31bad646f
BuildTools <unconfigured@null.spigotmc.org>
2023-02-18 23:34:16 -0500

    Some more capabilites and clean up

--- 2023_Robot_Lights_V4/2023_Robot_Lights_V4.ino
+++ 2023_Robot_Lights_V4/2023_Robot_Lights_V4.ino
@@ -1,4 +1,4 @@
-// IF THE LEDs ARE ACTING REALLY STRANGE FOR SEEMINGLY NEW REASON, TRY A NEW BATTERY
+// IF THE LEDs ARE ACTING REALLY STRANGE FOR SEEMINGLY NO REASON, TRY A NEW BATTERY
 /*
    1/31/23
    Program to drive up to 3 serial chains of RGB LEDs.
@@ -16,22 +16,6 @@
    female PWM ouptut connectors using 100 ohm resistors.  The resistors are
    not technically needed but were included to protect the Nano from
    possible wiring abuse.
-   
-        =============
-        LED TEST MODE
-        =============
-  
-   To enter a test mode and test a NeoPixel string, send a serial string to
-   the Nano on the USB port, following these rules:
-
-   The first character must be a 't' or a 'T'.  This enters the test mode.
-   The second character must be a '0' or '1' or '2' indicating which string will be tested.
-   If you want to use the pre-defined NeoPixel count in the program, send the string.
-   If you want to modify the pre-defined NeoPixe count,
-      the third character must be a space 
-      followed by the number of NeoPixels to be tested. 
-
-   The test mode will automatically be exited when the NeoPixel string test is complete.
 */
 
 #include <Adafruit_NeoPixel.h>
@@ -71,6 +55,7 @@ void fillShift(char* command, char* redValue, char* greenValue, char* blueValue)
 void shift();
 void shiftWrap();
 void flash(char* command, char* redValue, char* greenValue, char* blueValue);
+void test();
 //==============================================================
 void setup()
 //==============================================================
@@ -84,9 +69,6 @@ void setup()
   Serial.println("=============================================");
 
   Serial.println("Program Started");
-  Serial.println("Enter 'T'String Number(0, 1, or 2) [Space] Number of NeoPixels");
-
-
 }
 
 //==============================================================
@@ -97,10 +79,8 @@ void loop()         //=============== LOOP =====================
   {
     parseSerialCommand();
   }
-  //testLEDs(0);
 }
-
-//=================  END OF LOOP ===============================
+//==============================================================
 
 
 
@@ -108,10 +88,6 @@ void loop()         //=============== LOOP =====================
 void parseSerialCommand()
 //==============================================================
 {
-  int testMode = 0;
-  int testString = 0;
-  int testPixNum = 0;
-
   delay (50);   //Delay for hardware to accept the complete string
   int availableBytes = Serial.available();
 
@@ -130,222 +106,42 @@ void parseSerialCommand()
     return -1;
   }
 
-
-  if ( atoi(&command[SERIALPOS]) == 255 )
-  {
-    getInputColors(command, redValue, greenValue, blueValue);
-    fillAll(redValue, greenValue, blueValue);
+  switch ( atoi(&command[SERIALPOS]) ) {
+    case 150:
+      test();
+      break;
+    case 249:
+      flash(command, redValue, greenValue, blueValue);
+      break;
+    case 250:
+      shiftWrap();
+      break;   
+    case 251:
+      shift();
+      break;
+    case 252:
+      memcpy(redValue, &command[0], 3);
+      memcpy(greenValue, &command[3], 3);
+      memcpy(blueValue, &command[6], 3);   
+      fillShift(command, redValue, greenValue, blueValue);
+      break;
+    case 253:
+      switchGradient(command, redValue, greenValue, blueValue);
+      break;    
+    case 254:
+      fillRainbow();
+      break;
+    case 255:
+      getInputColors(command, redValue, greenValue, blueValue);
+      fillAll(redValue, greenValue, blueValue);
+      break;
+    default:     
+      break;
   }
-  else if ( atoi(&command[SERIALPOS]) == 254 )
-  {
-    fillRainbow();      
+  if ( atoi(&command[SERIALPOS]) < 150 ) {
+      getInputColors(command, redValue, greenValue, blueValue);
+      setSpecificLED(command, redValue, greenValue, blueValue);    
   }
-  else if ( atoi(&command[SERIALPOS]) == 253 )
-  {
-    switchGradient(command, redValue, greenValue, blueValue);
-  }
-  else if ( atoi(&command[SERIALPOS]) == 252 )
-  {
-    memcpy(redValue, &command[0], 3);
-    memcpy(greenValue, &command[3], 3);
-    memcpy(blueValue, &command[6], 3);   
-    fillShift(command, redValue, greenValue, blueValue);
-  }
-  else if ( atoi(&command[SERIALPOS]) == 251)
-  {
-    shift();
-  }
-  else if ( atoi(&command[SERIALPOS]) == 250 )
-  {
-    shiftWrap();  
-  }
-  else if ( atoi(&command[SERIALPOS]) == 249 )
-  {
-    flash(command, redValue, greenValue, blueValue);
-  }
-  else
-  {
-    getInputColors(command, redValue, greenValue, blueValue);
-    setSpecificLED(command, redValue, greenValue, blueValue);
-  }
-
-  //Get the string number to test
-  if (testMode)
-  {
-    Serial.println("Test Mode Entered");
-    switch (command[1])         // Second character in command must be 0, 1, or 2
-    {
-      case '0':
-      case '1':
-      case '2':
-        testString = atoi(&command[1]);  //Test a string of NeoPixels
-        Serial.print ("testString = ");
-        Serial.println(testString);
-        break;
-      default://Default Case
-        testMode = 0;
-        break;
-    }
-  }
-
-
-  //Check the space after the string number to test
-  if (testMode)
-  {
-    if (command[2] == ' ')
-    {
-      Serial.println("Space Found");
-
-      //Find the number of NeoPixels to be tested
-      //    int charLen = strlen(command);
-
-      Serial.println("Find NeoPixel Count");
-      testPixNum = atoi(&command[3]);
-      Serial.print("Number of NeoPixels to Test = ");
-      Serial.println(testPixNum);
-
-      if (testPixNum == 0)
-      {
-        testMode = 0;
-      }
-      else
-      {
-        numPixels[testString] = testPixNum;
-      }
-    }
-    else
-    {
-      Serial.print("Space NOT Found. Testing the default number of ");
-      Serial.print(numPixels[testString]);
-      Serial.println(" NeoPixels.");
-    }
-  }
-
-  //Test the Pixel String
-  if (testMode)
-  {
-    testLEDs(testString);
-
-    Serial.println();
-    Serial.println("Enter 'T'String Number(0, 1, or 2) [Space] Number of NeoPixels");
-
-  }
-}  // ========= End of Serial Command Parsing ===============
-
-
-
-//==============================================================
-void testLEDs(int stringNum)
-//==============================================================
-{
-
-  //Test the indicated light string
-  //Sequentially test the RED LED's, then the GREEN LED's and last the BLUE LED's
-
-  // lightString_x.Color takes GRB values, from t0 20,0,0 up to 255,255,255
-  int brightness = 50;
-
-  for (int y = 0; y < 3; y++)
-  {
-    for (int x = 0; x < numPixels[stringNum]; x++)
-    {
-      switch (atoi(&command[1]))
-      {
-        case 0:
-          {
-            lightString_0.setPixelColor(x, lightString_0.Color((y == 0)*brightness, (y == 2)*brightness, (y == 1)*brightness)); // Moderately bright G-R-B color.
-            lightString_0.show(); // This sends the updated pixel color to the hardware.
-            digitalWrite(LED_BUILTIN, x == 0); // turn the LED on if start of the sequence
-            delay(delayval); // Delay for a period of time (in milliseconds).
-            lightString_0.clear();
-            lightString_0.show(); // This sends the updated pixel color to the hardware.
-            break;
-          }
-        case 1:
-          {
-            //      Serial.print("x = ");
-            //    Serial.print(x);
-            //      Serial.print("  y = ");
-            //    Serial.println(y);
-            lightString_1.setPixelColor(x, lightString_1.Color((y == 0)*brightness, (y == 2)*brightness, (y == 1)*brightness)); // Moderately bright G-R-B color.
-            lightString_1.show(); // This sends the updated pixel color to the hardware.
-            digitalWrite(LED_BUILTIN, x == 0); // turn the LED on if start of the sequence
-            delay(delayval); // Delay for a period of time (in milliseconds).
-            lightString_1.clear();
-            lightString_1.show(); // This sends the updated pixel color to the hardware.
-            break;
-          }
-        case 2:
-          {
-            lightString_2.setPixelColor(x, lightString_2.Color((y == 0)*brightness, (y == 2)*brightness, (y == 1)*brightness)); // Moderately bright G-R-B color.
-            lightString_2.show(); // This sends the updated pixel color to the hardware.
-            digitalWrite(LED_BUILTIN, x == 0); // turn the LED on if start of the sequence
-            delay(delayval); // Delay for a period of time (in milliseconds).
-            lightString_2.clear();
-            lightString_2.show(); // This sends the updated pixel color to the hardware.
-            break;
-          }
-      }
-    }
-    delay(delayval * 2); // Delay for a period of time (in milliseconds).
-  }
-}
-
-//==============================================================
-void clearAndPause(int pauseMS )
-//==============================================================
-{
-  lightString_0.clear();
-  lightString_0.show();
-  delay(pauseMS);
-}
-
-
-//==============================================================
-void displayLine(uint8_t lineNum, uint8_t green, uint8_t red, uint8_t blue)
-//==============================================================
-{
-  // Display a line of one color of pixels from the center to the edge
-  // lineNum = 0 for straight down, then counting CCW
-  // green = the intensity of the GREEN led range is 0 to 255
-  // red = the intensity of the RED led range is 0 to 255
-  // blue = the intensity of the BLUE led range is 0 to 255
-
-  // for (int x = 0; x < LINE_LED; x++)
-  {
-
-    // lightString_0.Color takes GRB values, from 0,0,0 up to 255,255,255
-    //  lightString_0.setPixelColor(line[lineNum][x], pixels.Color(green, red, blue));
-  }
-}
-
-// ===================================================
-void randomSeed()
-//==============================================================
-{
-  // This routine will seed the random number generator
-  randomSeed(micros() + millis() + analogRead(0));
-
-
-}
-// ===================================================
-int randomNum(int maxNum)
-//==============================================================
-{
-  // This routine will return an integer between 0 and maxNum, inclusive
-
-
-  int x;
-
-  x = random(0, maxNum + 1); // Generate a random number (from 0 to n-1)...
-  /*
-    Serial.print("maxNum = ");
-    Serial.print(maxNum);
-    Serial.print("  LED Selected = ");
-    Serial.println(x);
-  */
-  return x;    //return LED index (1 to maxNum; index 0 is all off)
-
 }
 //=========================-Functions-=================================
 
@@ -480,4 +276,35 @@ void flash(char* command, char* redValue, char* greenValue, char* blueValue)
     lightString_0.show();
     delay(650);
   }
+}
+
+void test()
+{
+  /*
+  //Test fillAll
+  char command[] = "255000000255";
+  getInputColors(command, redValue, greenValue, blueValue);
+  fillAll(redValue, greenValue, blueValue);
+  delay(1000);
+  //Test setSpecificLED
+  char command[] = "2552552550";
+  getInputColors(command, redValue, greenValue, blueValue);
+  setSpecificLED(command, redValue, greenValue, blueValue); 
+  delay(650);
+  command[12] = "0002550001";
+  getInputColors(command, redValue, greenValue, blueValue);
+  setSpecificLED(command, redValue, greenValue, blueValue); 
+  delay(650);
+  command[12] = "0000002552";
+  getInputColors(command, redValue, greenValue, blueValue);
+  setSpecificLED(command, redValue, greenValue, blueValue); 
+  delay(1000);
+  //Test rainbow and shift functions
+  fillRainbow();
+  delay(650);
+  shift();
+  delay(650);
+  fillRainbow();
+  shiftWrap();
+  */
 }
